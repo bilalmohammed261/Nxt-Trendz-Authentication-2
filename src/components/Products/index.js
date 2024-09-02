c2a2c28df0fb179d101b7443afb54a0b5f827cd7
Bilal <bilalmohammed261@gmail.com>
2024-09-02 08:49:38 +0000

    built the app

---
+++ src/components/Products/index.js
@@ -0,0 +1,29 @@
+// Write your JS code here
+
+import Cookies from 'js-cookie'
+import Header from '../Header'
+
+import './index.css'
+
+const Products = props => {
+  const {history} = props
+  const jwtToken = Cookies.get('jwt_token')
+  if (jwtToken === undefined) {
+    history.replace('/login')
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="products-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+          alt="products"
+          className="products-img"
+        />
+      </div>
+    </>
+  )
+}
+
+export default Products
